4d62a163467bd258d4b66161fd0a5105f8941ee2
D3N2-A <anmolsharma999937@gmail.com>
2022-07-12 10:10:22 +0530

    useState counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "John",
+    age: 24,
+    message: "Random message",
+  });
+  const clickHandler = () => {
+    setPerson({ ...person, message: "hello world" });
+    if (person.message === "Random message") {
+    } else {
+      setPerson({ ...person, message: "Random message" });
+    }
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" type="button" onClick={clickHandler}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
