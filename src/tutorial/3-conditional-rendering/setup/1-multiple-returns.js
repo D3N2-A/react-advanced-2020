6f757174a6c1c36f2e8c9e22bfffcbf8345c6c71
D3N2-A <anmolsharma999937@gmail.com>
2022-07-13 19:46:25 +0530

    shortCircuits

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,53 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [user, setUser] = useState("default user");
+  let erCode = 200;
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          erCode = resp.status;
+          console.log(resp);
+          return resp.json();
+        } else {
+          setLoading(false);
+          setError(true);
+        }
+      })
+      .then((data) => {
+        const { login } = data;
+        setLoading(false);
+        setUser(login);
+        // console.log(login);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading ........</h1>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error {erCode}!!!!</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <h1>{user}</h1>
+    </>
+  );
 };
 
 export default MultipleReturns;
