fa6bf515e8ce4a2a4b87c2fe32e74e2743b355a4
D3N2-A <anmolsharma999937@gmail.com>
2022-07-13 20:10:13 +0530

    ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+  const [isError, setIsError] = useState(false);
+  return (
+    <>
+      <h2>Hello world</h2>
+      <button
+        className="btn"
+        onClick={() => {
+          setIsError(!isError);
+        }}
+      >
+        Toggle Error
+      </button>
+      {isError && <h3>Error....</h3>}
+      {isError ? (
+        <p>there is error</p>
+      ) : (
+        <div>
+          <h2>hello boiiii</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
