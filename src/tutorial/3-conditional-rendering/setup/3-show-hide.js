d475db2c8a0d54c695bf639e831b11e604cfe7e4
D3N2-A <anmolsharma999937@gmail.com>
2022-07-14 09:06:37 +0530

    form control

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+  const [show, setShow] = useState(false);
 
+  return (
+    <>
+      <button
+        className="btn"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  return (
+    <div>
+      <h2>Window</h2>
+      <h3>Size: {size}</h3>
+    </div>
+  );
+};
 export default ShowHide;
