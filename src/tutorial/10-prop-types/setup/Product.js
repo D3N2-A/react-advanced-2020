65083f2b829c5333899a6e898ba2e8ca0ce8cbe4
D3N2-A <anmolsharma999937@gmail.com>
2022-07-18 09:19:57 +0530

    react router

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ id, image, price, name }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <img src={url || defaultImage} alt="" />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
 
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
+Product.defaultProps = {
+  image: defaultImage,
+
+  name: "NaN",
+  id: 12093810991,
+};
 export default Product;
